6810153f50862a54560d14647fb97098f0013ba2
Jayraj Ladkat <jayrajkladkat@gmail.com>
2024-12-01 19:25:24 +0530

    Add project files.

---
+++ Program.cpp
@@ -0,0 +1,46 @@
+
+#include "LoginForm.h"
+#include "MainForm.h"
+#include "RegisterForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	User^ user = nullptr;
+	while (true) {
+		Project1 ::LoginForm loginForm;
+		loginForm.ShowDialog();
+
+		if (loginForm.switchToRegister) {
+			//show the register form
+			Project1 ::RegisterForm registerForm;
+			registerForm.ShowDialog();
+
+			if (registerForm.switchToLogin) {
+				continue;
+			}
+			else {
+				user = registerForm.user;
+				break;
+			}
+		}
+		else {
+			user = loginForm.user;
+			break;
+		}
+	}
+
+	if (user != nullptr) {
+		Project1 ::MainForm mainForm(user);
+		Application::Run(% mainForm);
+	}
+	else {
+		MessageBox::Show("Authentication Canceled",
+			"Program.cpp", MessageBoxButtons::OK);
+	}
+}
